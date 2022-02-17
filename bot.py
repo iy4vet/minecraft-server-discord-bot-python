36b1d3daafd7a975c7dec1e0c806a55402bd6d65
therectifier <96103963+therectifier@users.noreply.github.com>
2022-02-17 19:25:39 +0530

    Slash commmands dev (#5)
    * add command /say <message>, multiple server ops allowed
    
    * minor changes, update to make the bot ping all server-ops
    
    * Added slash commands! Allowed multiple server-ops. Removed hardcoded ports.
    
    Moved to slash commands. Multiple server operators can be set in `server-op` by separating each user id with a comma. Hardcoded ports are removed and a custom server port can be set in `server-address`.

--- bot.py
+++ bot.py
@@ -1,39 +1,38 @@
-#https://github.com/nottherectifier
+#https://github.com/therectifier
 
+import asyncio
 import disnake
-import dotenv
 import os
-import random
 import socket
-import string
 import threading
 import time
 import urllib.request
+from disnake.ext import commands
 from dotenv import load_dotenv
 from mcrcon import MCRcon
 
-mcHost = disnake.Client()
 load_dotenv(dotenv_path=f"{os.getcwd()}/bot.env")
 load_dotenv(dotenv_path=f"{os.getcwd()}/server.properties")
-ops = os.getenv("server-op").split(',')
+waitmsg = "The server is either starting up, or shutting down. Please wait a bit, and then try again. "
 running = False
 error = False
-waitmsg = "The server is either starting up, or shutting down. Please wait a bit, and then try again. "
-
-
+try:
+    serverport = os.getenv("server-address").split(":")[1]
+except IndexError:
+    serverport = 25565
 if os.getenv("enable-rcon") != "true":
     print("Rcon is not enabled in your server.properties file. Please change enable-rcon to true. ")
-    error = True
-if os.getenv("rcon.port") != "25575":
-    print("Rcon port is not set to 25575 in your server.properties file. Please change rcon.port to 25575. ")
-    print("If you have any port forward rules for the Rcon port, please change them accordingly. ")
     error = True
 if os.getenv("rcon.password") == "":
     print("No password was set for Rcon. Please set a password for Rcon under rcon.password")
     error = True
 if error:
     time.sleep(10)
-    exit
+    exit(code=0)
+
+mcBot = commands.Bot(
+    sync_commands_debug=True,
+)
 
 def server():
     global running
@@ -41,28 +40,34 @@ def server():
     print("Server started. ")
     os.system(os.getenv("start-script"))
     running = False
-    time.sleep(1)
     print("Server stopped. ")
 
 def rcon(cmd):
-    mcr = MCRcon(os.getenv("server-ip"),os.getenv("rcon.password"))
+    mcr = MCRcon(os.getenv("server-ip"),os.getenv("rcon.password"),port=int(os.getenv("rcon.port")))
     try:
         mcr.connect()
         resp = mcr.command(cmd)
         mcr.disconnect()
+        print("Rcon successful! ")
         return resp
     except ConnectionRefusedError:
+        print("Rcon unsuccessful: Server in start/stop state. ")
         return (waitmsg)
 
-def ping(ip):
+def ping(ip,port):
     if not running:
+        print("Ping failed: Server down. ")
         return -1
     if not rcon("list").startswith("There are"):
+        print("Ping failed: Server in start/stop state. ")
         return -2
     try:
-        return socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((ip,25565))
+        print("Pinging! ")
+        return socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((ip,port))
     except:
+        print("Ping failed: Sockets error. ")
         return -3
+
 def shutdown():
     time.sleep(300)
     print("Auto shutdown thread started. ")
@@ -71,87 +76,132 @@ def shutdown():
         rcon("execute unless entity @a run stop")
         time.sleep(180)
 
-@mcHost.event
+@mcBot.event
 async def on_ready():
-    print("Logged in as {0.user}".format(mcHost))
-    await mcHost.change_presence(activity=disnake.Game("$help for help"))
+    print("Logged in as {0.user}".format(mcBot))
+    await mcBot.change_presence(activity=disnake.Game("/help for help"))
 
-@mcHost.event
-async def on_message(message):
-    msg = message.content
-    ctx = message.channel
-    if msg == "$start":
-        if running:
-            await ctx.send("Server is already running! ")
-        else:
-            await ctx.send("Starting server. ")
-            start_thread = threading.Thread(target=server)
-            stop_thread = threading.Thread(target=shutdown)
-            start_thread.start()
-            stop_thread.start()
-    if msg == "$stop":
-        if running:
-            print("Attempting manual shutdown... ")
-            await ctx.send("Attempting to stop the server. ")
-            try:
-                await ctx.send(rcon("execute unless entity @a run stop"))
-            except:
-                pass
-        else:
-            await ctx.send("Server is already down. ")
-    if msg == "$check" or msg == "$ip" or msg == "$list":
-        await ctx.send("This command is deprecated. Use `$info` for server information. ")
-    if msg == "$info":
-        online = ""
-        if running:
-            online=rcon("list")
-        await ctx.send("Server address: `"+os.getenv("server-address")+"`. \nServer running: "+str(running)+". \n"+online)
-    if msg == "$ip-check":
-        await ctx.send("I will check the address. ")
-        connectcode = ping(os.getenv("server-address"))
+@mcBot.slash_command(description="Starts the server")
+async def start(inter):
+    await inter.response.defer()
+    if running:
+        await inter.edit_original_message(content="Server is already running! ")
+    else:
+        threading.Thread(target=server).start()
+        while (not rcon("list").startswith("There are")) and running:
+            await asyncio.sleep(5)
+        threading.Thread(target=shutdown).start()
+        await inter.edit_original_message(content="Started server. ")
+
+@mcBot.slash_command(description="Attempts to stop the server")
+async def stop(inter):
+    await inter.response.defer()
+    if running:
+        print("Attempting manual shutdown... ")
+        try:
+            await inter.edit_original_message(content=rcon("execute unless entity @a run stop"))
+        except disnake.errors.HTTPException:
+            await inter.edit_original_message(content="Could not stop the server. ")
+    else:
+        await inter.edit_original_message(content="Server is already down. ")
+
+@mcBot.slash_command(description="Gets server information")
+async def info(inter):
+    await inter.response.defer()
+    online = ""
+    if running:
+        online=rcon("list")
+    await inter.edit_original_message(content="Server address: `"+os.getenv("server-address")+"`. \nServer running: "+str(running)+". \n"+online)
+
+@mcBot.slash_command(description="Checks server address")
+async def ipcheck(inter):
+    await inter.response.defer()
+    connectcode = ping(os.getenv("server-address").split(":")[0],serverport)
+    if connectcode == 0:
+        await inter.edit_original_message(content="I am able to ping the server at `"+os.getenv("server-address")+"`. \nCheck that the address you set in Minecraft matches this. If it's correct, check your network connection and firewall settings. ")
+    elif connectcode == -1:
+        await inter.edit_original_message(content="The server is not running. Please start the server to check the address. ")
+    elif connectcode == -2:
+        await inter.edit_original_message(waitmsg)
+    else:
+        extip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+        connectcode = ping(extip,serverport)
         if connectcode == 0:
-            await ctx.send("I am able to ping the server at `"+os.getenv("server-address")+"`. \nCheck that the address you set in Minecraft matches this. If it's correct, check your network connection and firewall settings. ")
-        elif connectcode == -1:
-            await ctx.send("The server is not running. Please start the server to check the address. ")
-        elif connectcode == -2:
-            await ctx.send(waitmsg)
-        else:
-            await ctx.send("The address is incorrect. I will attempt to refresh it now. ")
-            extip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-            connectcode = ping(extip)
-            if connectcode == 0:
-                await ctx.send("New IP: "+extip)
+            await inter.edit_original_message(content="New IP: "+extip)
+            if serverport == 25565:
                 os.environ["server-address"] = extip
             else:
-                opping = ""
-                for i in ops:
-                    opping = opping + str("<@"+i+"> ")
-                await ctx.send("I was unable to get a new working address for the server. Please ask the bot host to update the `server-address` field and restart the bot. ")
-                await ctx.send("Alternatively, ask "+opping+"to use the $ip-set command to update the server address")
-                await ctx.send("However, you may try this address: `"+extip+"`. ")
-    if msg.startswith("$say ") and running:
-        try:
-            await ctx.send(rcon('tellraw @a ["{'+str(await mcHost.fetch_user(message.author.id))+'} '+str(msg[5:])+'"]'))
-        except:
-            pass
-    if msg == "$help":
-        await ctx.send("There are 4 commands that are available to use. \nTo start the server, use `$start`. \nTo get server information, use `$info`. \nTo manually stop the server, use `$stop`. This will only stop the server if no players are online. \n $say <message> will send your message in the Minecraft server if it's running. \nIf the address given in `$info` does not work, you can do `$ip-check`. This will update the server address if the current one does not work. ")
-    if msg.startswith("$ip-set ") and str(message.author.id) in ops:
-        await ctx.send("Checking this address. ")
-        connectcode = ping(msg[8:])
-        if connectcode == 0:
-            await ctx.send("This address works. I will update it now. ")
-            os.environ["server-address"] = msg[8:]
-        elif connectcode == -1:
-            await ctx.send("The server is not running. Please start the server so I can check that this address works. ")
-        elif connectcode == -2:
-            await ctx.send(waitmsg)
+                os.environ["server-address"] = extip + ":" + serverport
         else:
-            await ctx.send("This address doesn't work. I will continue with the old one. ")
-    if msg.startswith("$rcon ") and str(message.author.id) in ops:
-        if running:
-            await ctx.send("Rcon: "+rcon(msg[6:]))
-        else:
-            await ctx.send("Please start the server to execute this command. ")
+            opping = ""
+            for i in os.getenv("server-op").split(','):
+                opping = opping + str("<@"+i+"> ")
+            await inter.edit_original_message(content="I was unable to get a new working address for the server. Please ask the bot host to update the `server-address` field and restart the bot. \nAlternatively, ask "+opping+"to use the $ip-set command to update the server address. \nHowever, you may try this address: `"+extip+"`. ")
 
-mcHost.run(os.getenv("bot-token"))
+@mcBot.slash_command(description="Sends a message in the Minecraft server")
+async def say(inter,message:str):
+    """
+    Sends a message in the Minecraft server
+
+    Parameters
+    ----------
+    message: The message to be sent
+    """
+    await inter.response.defer(ephemeral=True)
+    try:
+        await inter.edit_original_message(content=rcon('tellraw @a ["{'+str(await mcBot.fetch_user(inter.author.id))+'} '+message+'"]'))
+    except disnake.errors.HTTPException: 
+        await inter.edit_original_message(content="Sent! ")
+
+@mcBot.slash_command(description="Get help on all commands")
+async def help(inter):
+    await inter.response.send_message("There are 5 commands that are available to use. \nTo start the server, use `/start`. \nTo get server information, use `/info`. \nTo manually stop the server, use `/stop`. This will only stop the server if no players are online. \nTo send a message in the server, use `/say <message>`. \nIf the address given in `/info` does not work, you can do `/ipcheck`. This will update the server address if the current one does not work. ")
+
+@mcBot.slash_command(description="Sets a new address for the Minecraft server")
+async def ipset(inter,address:str,port:int = serverport):
+    """
+    Sets a new address for the Minecraft server
+
+    Parameters
+    ----------
+    address: The new server address
+    port: The new server port (optional)
+    """
+    if str(inter.author.id) not in os.getenv("server-op").split(','):
+        await inter.response.send_message(content="You don't have access to this command! ",ephemeral=True)
+        return
+    await inter.response.defer()
+    connectcode = ping(address,port)
+    if connectcode == 0:
+        await inter.edit_original_message(content="This address works. I will update it now. ")
+        if port == 25565:
+            os.environ["server-address"] = address
+        else:
+            os.environ["server-address"] = address + ":" + port
+            serverport = port
+    elif connectcode == -1:
+        await inter.edit_original_message(content="The server is not running. Please start the server so I can check that this address works. ")
+    elif connectcode == -2:
+        await inter.edit_original_message(content=waitmsg)
+    else:
+        await inter.edit_original_message(content="This address doesn't work. I will continue with the old one. ")
+
+@mcBot.slash_command(description="Executes a Minecraft command on the Minecraft server")
+async def cmd(inter,command:str):
+    """
+    Executes a Minecraft command on the Minecraft server
+
+    Parameters
+    ----------
+    command: The command to be executed
+    """
+    if str(inter.author.id) not in os.getenv("server-op").split(','):
+        await inter.response.send_message(content="You don't have access to this command! ",ephemeral=True)
+        return
+    await inter.response.defer()
+    if running:
+        await inter.edit_original_message(content="Rcon: "+rcon(command))
+    else:
+        await inter.edit_original_message(content="Please start the server to execute this command. ")
+
+mcBot.run(os.getenv("bot-token"))
